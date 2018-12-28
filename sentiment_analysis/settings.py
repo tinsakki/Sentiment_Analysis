f12af0718fa16300a81dab9ea2415810d5423349
CORP\ajain <ajain@measureone.com>
2018-12-28 21:33:08 +0530

    Committed the code

--- sentiment_analysis/settings.py
+++ sentiment_analysis/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+
 
 ]
 
